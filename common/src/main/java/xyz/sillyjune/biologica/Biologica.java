726fad548d5721ff98ab4ea940cf2fdf63e4ae77
Juniper Gardiner <juniper.gardiner@gmail.com>
2025-08-27 00:04:34 +0100

    Textures for puffin beaks, rattle and rhino horns

--- common/src/main/java/xyz/sillyjune/biologica/Biologica.java
+++ common/src/main/java/xyz/sillyjune/biologica/Biologica.java
@@ -5,5 +5,6 @@ public final class Biologica {
 
     public static void init() {
         // Write common init code here.
+        Items.register();
     }
 }
--- common/src/main/java/xyz/sillyjune/biologica/Items.java
+++ common/src/main/java/xyz/sillyjune/biologica/Items.java
@@ -1,4 +1,19 @@
 package xyz.sillyjune.biologica;
 
+import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.Item;
+
+import static xyz.sillyjune.biologica.Biologica.MOD_ID;
+
 public class Items {
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registries.ITEM);
+    public static final RegistrySupplier<Item> OX_HORN = ITEMS.register("ox_horn", () -> new Item(new Item.Properties()));
+    public static final RegistrySupplier<Item> FANG = ITEMS.register("fang", () -> new Item(new Item.Properties()));
+    public static final RegistrySupplier<Item> CHITIN = ITEMS.register("chitin", () -> new Item(new Item.Properties()));
+
+    public static void register() {
+        ITEMS.register();
+    }
 }
