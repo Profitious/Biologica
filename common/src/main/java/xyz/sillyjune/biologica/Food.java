589dcea6cc953dec91d0fbf3c874f9b475379b83
Juniper Gardiner <juniper.gardiner@gmail.com>
2025-08-27 00:40:38 +0100

    Finish adding sauropod meat

--- common/src/main/java/xyz/sillyjune/biologica/Food.java
+++ common/src/main/java/xyz/sillyjune/biologica/Food.java
@@ -5,4 +5,5 @@ import net.minecraft.world.food.FoodProperties;
 public class Food {
     public static final FoodProperties MAMMOTH_MEAT_FOOD = (new FoodProperties.Builder()).nutrition(8).saturationMod(0.6F).build();
     public static final FoodProperties MAHI_MAHI_MEAT_FOOD = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.4F).build();
+    public static final FoodProperties SAUROPOD_MEAT_FOOD = (new FoodProperties.Builder()).nutrition(8).saturationMod(0.6F).build();
 }
--- common/src/main/java/xyz/sillyjune/biologica/Items.java
+++ common/src/main/java/xyz/sillyjune/biologica/Items.java
@@ -12,7 +12,7 @@ public class Items {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registries.ITEM);
     public static final RegistrySupplier<Item> MAMMOTH_MEAT = ITEMS.register("mammoth_meat", () -> new Item(new Item.Properties().food(MAMMOTH_MEAT_FOOD)));
     public static final RegistrySupplier<Item> MAHI_MAHI_MEAT = ITEMS.register("mahi_mahi_meat", () -> new Item(new Item.Properties().food(MAHI_MAHI_MEAT_FOOD)));
-
+    public static final RegistrySupplier<Item> SAUROPOD_MEAT = ITEMS.register("sauropod_meat", () -> new Item(new Item.Properties().food(SAUROPOD_MEAT_FOOD)));
 
     public static final RegistrySupplier<Item> OX_HORN = ITEMS.register("ox_horn", () -> new Item(new Item.Properties()));
     public static final RegistrySupplier<Item> FANG = ITEMS.register("fang", () -> new Item(new Item.Properties()));
