09ff1bad9634a4b54620ef09e2d1f1611857660f
Juniper Gardiner <juniper.gardiner@gmail.com>
2025-08-26 23:30:09 +0100

    Initial commit with architectury project

---
+++ common/src/main/java/xyz/sillyjune/biologica/Biologica.java
@@ -0,0 +1,9 @@
+package xyz.sillyjune.biologica;
+
+public final class Biologica {
+    public static final String MOD_ID = "biologica";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ fabric/src/main/java/xyz/sillyjune/biologica/fabric/client/BiologicaFabricClient.java
@@ -0,0 +1,10 @@
+package xyz.sillyjune.biologica.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class BiologicaFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
