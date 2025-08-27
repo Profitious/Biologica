a6f6ba09bc4da552473b6953de2c9290c97e844f
Juniper Gardiner <juniper.gardiner@gmail.com>
2025-08-27 01:01:42 +0100

    Add raw meats and textures

--- common/src/main/java/xyz/sillyjune/biologica/Food.java
+++ common/src/main/java/xyz/sillyjune/biologica/Food.java
@@ -4,6 +4,13 @@ import net.minecraft.world.food.FoodProperties;
 
 public class Food {
     public static final FoodProperties MAMMOTH_MEAT_FOOD = (new FoodProperties.Builder()).nutrition(8).saturationMod(0.6F).build();
+    public static final FoodProperties RAW_MAMMOTH_MEAT_FOOD = (new FoodProperties.Builder()).nutrition(8).saturationMod(0.6F).build();
+
     public static final FoodProperties MAHI_MAHI_MEAT_FOOD = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.4F).build();
+    public static final FoodProperties RAW_MAHI_MAHI_MEAT_FOOD = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.4F).build();
+
     public static final FoodProperties SAUROPOD_MEAT_FOOD = (new FoodProperties.Builder()).nutrition(8).saturationMod(0.6F).build();
+    public static final FoodProperties RAW_SAUROPOD_MEAT_FOOD = (new FoodProperties.Builder()).nutrition(8).saturationMod(0.6F).build();
+
+
 }
--- common/src/main/java/xyz/sillyjune/biologica/Items.java
+++ common/src/main/java/xyz/sillyjune/biologica/Items.java
@@ -11,8 +11,13 @@ import static xyz.sillyjune.biologica.Food.*;
 public class Items {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registries.ITEM);
     public static final RegistrySupplier<Item> MAMMOTH_MEAT = ITEMS.register("mammoth_meat", () -> new Item(new Item.Properties().food(MAMMOTH_MEAT_FOOD)));
+    public static final RegistrySupplier<Item> RAW_MAMMOTH_MEAT = ITEMS.register("raw_mammoth_meat", () -> new Item(new Item.Properties().food(RAW_MAMMOTH_MEAT_FOOD)));
+
     public static final RegistrySupplier<Item> MAHI_MAHI_MEAT = ITEMS.register("mahi_mahi_meat", () -> new Item(new Item.Properties().food(MAHI_MAHI_MEAT_FOOD)));
+    public static final RegistrySupplier<Item> RAW_MAHI_MAHI_MEAT = ITEMS.register("raw_mahi_mahi_meat", () -> new Item(new Item.Properties().food(RAW_MAHI_MAHI_MEAT_FOOD)));
+
     public static final RegistrySupplier<Item> SAUROPOD_MEAT = ITEMS.register("sauropod_meat", () -> new Item(new Item.Properties().food(SAUROPOD_MEAT_FOOD)));
+    public static final RegistrySupplier<Item> RAW_SAUROPOD_MEAT = ITEMS.register("raw_sauropod_meat", () -> new Item(new Item.Properties().food(RAW_SAUROPOD_MEAT_FOOD)));
 
     public static final RegistrySupplier<Item> OX_HORN = ITEMS.register("ox_horn", () -> new Item(new Item.Properties()));
     public static final RegistrySupplier<Item> FANG = ITEMS.register("fang", () -> new Item(new Item.Properties()));
