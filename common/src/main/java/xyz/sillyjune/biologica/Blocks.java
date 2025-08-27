39d2e1da0b273534eb3290984ceb7c50ced4aee0
Juniper Gardiner <juniper.gardiner@gmail.com>
2025-08-27 11:53:14 +0100

    Add boston fern

--- common/src/main/java/xyz/sillyjune/biologica/Blocks.java
+++ common/src/main/java/xyz/sillyjune/biologica/Blocks.java
@@ -1,15 +1,23 @@
 package xyz.sillyjune.biologica;
 
 import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.TallGrassBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 
 import static xyz.sillyjune.biologica.Biologica.MOD_ID;
 
 public class Blocks {
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(MOD_ID, Registries.BLOCK);
 
-    public static void register() {
+    public static RegistrySupplier<Block> BOSTON_FERN = BLOCKS.register("boston_fern", () -> new TallGrassBlock(BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).replaceable().noCollission().instabreak().sound(SoundType.GRASS).offsetType(BlockBehaviour.OffsetType.XYZ).ignitedByLava().pushReaction(PushReaction.DESTROY)));;
 
+    public static void register() {
+        BLOCKS.register();
     }
 }
--- fabric/src/main/java/xyz/sillyjune/biologica/fabric/client/BiologicaFabricClient.java
+++ fabric/src/main/java/xyz/sillyjune/biologica/fabric/client/BiologicaFabricClient.java
@@ -1,10 +1,15 @@
 package xyz.sillyjune.biologica.fabric.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
+
+import static xyz.sillyjune.biologica.Blocks.BOSTON_FERN;
 
 public final class BiologicaFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.cutout(), BOSTON_FERN.get());
     }
 }
