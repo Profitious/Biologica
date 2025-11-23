48806537eff855cc8a09aca2effa6b5babe824ef
Daniel Orr <orrdaniel73@gmail.com>
2025-11-23 15:05:55 +0000

    fix: forge init

--- common/src/main/java/xyz/sillyjune/biologica/Biologica.java
+++ common/src/main/java/xyz/sillyjune/biologica/Biologica.java
@@ -1,11 +1,16 @@
 package xyz.sillyjune.biologica;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import xyz.sillyjune.biologica.index.BiologicaIndex;
 
 public final class Biologica {
     public static final String MOD_ID = "biologica";
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     public static void init() {
+        LOGGER.info("Initializing " + MOD_ID);
         BiologicaIndex.register();
     }
 }
--- forge/src/main/java/xyz/sillyjune/biologica/forge/BiologicaForge.java
+++ forge/src/main/java/xyz/sillyjune/biologica/forge/BiologicaForge.java
@@ -1,22 +1,29 @@
 package xyz.sillyjune.biologica.forge;
 
+import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import xyz.sillyjune.biologica.Biologica;
 import xyz.sillyjune.biologica.client.BiologicaClient;
 
-import static xyz.sillyjune.biologica.Biologica.MOD_ID;
-
-@Mod(MOD_ID)
+@Mod(Biologica.MOD_ID)
 public final class BiologicaForge {
     public BiologicaForge(FMLJavaModLoadingContext context) {
+        IEventBus bus = context.getModEventBus();
+        EventBuses.registerModEventBus(Biologica.MOD_ID, bus);
+        bus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
         Biologica.init();
     }
 
-    @Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = Biologica.MOD_ID, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
