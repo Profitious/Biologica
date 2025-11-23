601256866a7db9e5cb2959d81793030202f78524
Daniel Orr <orrdaniel73@gmail.com>
2025-11-23 12:58:17 +0000

    feat: datagen init

---
+++ fabric/src/main/java/xyz/sillyjune/biologica/data/BiologicaDataGenerator.java
@@ -0,0 +1,13 @@
+package xyz.sillyjune.biologica.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import xyz.sillyjune.biologica.data.server.BlockTagProvider;
+
+public class BiologicaDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        FabricDataGenerator.Pack pack = generator.createPack();
+        pack.addProvider(BlockTagProvider::new);
+    }
+}
